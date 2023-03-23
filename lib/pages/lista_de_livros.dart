b688775a97d75fafaf9c51da8c263545d5c78d61
Guilherme <guilhermemagno09@gmail.com>
2023-03-23 16:43:10 -0300

    adding divided line widget and finishing main page

--- lib/pages/lista_de_livros.dart
+++ lib/pages/lista_de_livros.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/divided_line.dart';
+
 class ListaDeLivros extends StatefulWidget {
   const ListaDeLivros({super.key});
   @override
@@ -11,8 +13,62 @@ class _ListaDeLivrosState extends State<ListaDeLivros> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
-      body: Center(
-        child: Text('This is Google Fonts'),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Lista de livros',
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        FloatingActionButton.small(
+                          onPressed: () {},
+                          child: const Icon(Icons.add),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const DividedLine(),
+                  ListView.separated(
+                      shrinkWrap: true,
+                      itemBuilder: (context, i) => const Padding(
+                            padding: EdgeInsets.only(left: 40),
+                            child: ListTile(
+                              title: Text("Livro qualquer"),
+                              subtitle: Text(
+                                'about some books',
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                      separatorBuilder: (context, i) => const Divider(
+                            color: Colors.black12,
+                            thickness: 1,
+                          ),
+                      itemCount: 5),
+                  const DividedLine(),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 30),
+              child: VerticalDivider(
+                color: Colors.black38,
+                thickness: 3,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/divided_line.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DividedLine extends StatelessWidget {
+  const DividedLine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Divider(
+      color: Colors.black12,
+      thickness: 1,
+    );
+  }
+}
